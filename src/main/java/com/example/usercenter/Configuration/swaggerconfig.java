5eff24b13597cf265da3955b2b3c10a545ab36f1
fanfan <2876709381@qq.com>
2024-01-10 22:59:41 +0800

    knife4j文档

--- src/main/java/com/example/usercenter/Configuration/swaggerconfig.java
+++ src/main/java/com/example/usercenter/Configuration/swaggerconfig.java
@@ -2,25 +2,25 @@ package com.example.usercenter.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 @Configuration
-@EnableOpenApi
+@EnableSwagger2WebMvc
 public class swaggerconfig {
     /**
      * 创建swagger接口bean实体
      * @return
      */
-    @Bean
+    @Bean(value="docket")
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
