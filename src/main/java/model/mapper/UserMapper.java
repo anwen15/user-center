f9f9c03a603534b4ed012b2517b1e637f9edc753
anwen15 <2876709381@qq.com>
2024-01-16 22:09:09 +0800

    登录改分布式session

---
+++ src/main/java/model/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package model.mapper;
+
+import model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author nicefang
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-01-15 00:12:13
+* @Entity model.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
